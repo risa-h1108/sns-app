25915095bd9407439992148db0c431f017c8af81
risa-h1108 <tusen.takk39@gmail.com>
2026-03-18 01:04:50 +0900

    user login display&feature

--- src/repositories/auth.jsx
+++ src/repositories/auth.jsx
@@ -1,4 +1,5 @@
 import { supabase } from "../lib/supabase";
+import Signin from "../pages/Signin";
 
 export const authRepository = {
   async signup(name, email, password) {
@@ -8,7 +9,16 @@ export const authRepository = {
       password,
       options: { data: { name } }, //「email,password以外の情報：ユーザー名（=name）」
     });
+    // if (error != null) :errorがnullではない（＝エラーが存在する）場合
     if (error != null) throw new Error(error.message);
+    return { ...data.user, userName: data.user.user_metadata.name };
+  },
+  async signin(email, password) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw new Error(error.message);
     return { ...data.user, userName: data.user.user_metadata.name };
   },
 };
