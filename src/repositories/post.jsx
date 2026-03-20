291b4dff79382cbc5449144aae2f94835ec50683
risa-h1108 <tusen.takk39@gmail.com>
2026-03-21 04:03:48 +0900

    feat:投稿されたデータをsupabaseへ送信する機能の実装

---
+++ src/repositories/post.jsx
@@ -0,0 +1,11 @@
+export const postRepository = {
+  async create(content, userId) {
+    const { data, error } = await supabase
+      .from("posts") //postsテーブルからget等を実施できるよう記載
+      .insert([{ content, user_id: userId }]) //insertで作成(追加)されたデータが定義したdataに入る
+      .select(); //全ての情報を取得
+
+    if (error != null) throw new Error(error.message);
+    return data[0];
+  },
+};
