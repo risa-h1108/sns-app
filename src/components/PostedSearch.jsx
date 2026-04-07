3dc6fa7250c5ef2460aa4cb381ebede4517281c8
risa-h1108 <tusen.takk39@gmail.com>
2026-04-07 23:52:43 +0900

    style:使用しないNameの削除

--- src/components/PostedSearch.jsx
+++ src/components/PostedSearch.jsx
@@ -7,9 +7,6 @@ export function PostedSearch() {
   return (
     <div className="bg-white p-4 rounded-lg shadow-md h-[200px] flex flex-col justify-center">
       <h2 className="text-xl font-semibold mb-4">投稿の検索</h2>
-      <p>
-        <strong>Name:</strong> {currentUser.userName}
-      </p>
     </div>
   );
 }
