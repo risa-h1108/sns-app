1309a73f401281abc93aec3212a215488af9c36b
risa-h1108 <tusen.takk39@gmail.com>
2026-04-09 00:49:10 +0900

    style:検索窓の入力箇所、検索アイコンの仮作成

--- src/components/PostedSearch.jsx
+++ src/components/PostedSearch.jsx
@@ -1,12 +1,27 @@
-import { useContext } from "react";
-import { SessionContext } from "../SessionProvider";
+// export function PostedSearch(props) {
+//   return (
+//     <section className="mb-10">
+//       <div className="bg-white p-4 rounded-lg shadow-md">
+//         <h2 className="text-xl font-semibold mb-4">投稿の検索</h2>
+//       </div>
+//     </section>
+//   );
+// }
 
-export function PostedSearch() {
-  const { currentUser } = useContext(SessionContext);
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+export function PostedSearch(props) {
   return (
-    <div className="bg-white p-4 rounded-lg shadow-md">
+    <section className="mb-10">
       <h2 className="text-xl font-semibold mb-4">投稿の検索</h2>
-    </div>
+      <input
+        className="bg-gray-700 w-1/3 p-2 rounded-l-lg focus:outline-none"
+        placeholder="探したい曲を入力してください"
+      />
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg">
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
+    </section>
   );
 }
